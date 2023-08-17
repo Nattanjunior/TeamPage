048302a789a6acf56008f81b9aec59e6db36f629
Nataniel Junior <fnatanieljunior@gmail.com>
2023-08-17 17:36:42 -0300

    Pastas: images,pages e style foram criadas

---
+++ TeamPage/src/pages/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import '../style/App.css'
+
+function App() {
+
+
+  return (
+   <div>
+    <h1>The creative crew</h1>
+    <span>
+      <h2>Who we are</h2>
+      <p>We are team of creatively diverse.  driven.  innovative individuals working in various locations from the world.</p>
+      </span>
+   </div>
+  )
+}
+
+export default App
