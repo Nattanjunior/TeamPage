06fd889dd0f2209a38388b665210f4f219a61ebd
Nataniel Junior <fnatanieljunior@gmail.com>
2023-08-19 21:50:25 -0300

    page quase finalizada...

--- TeamPage/src/pages/App.jsx
+++ TeamPage/src/pages/App.jsx
@@ -12,52 +12,47 @@ function App() {
 
   return (
    <div>
-    <h1>The creative crew</h1>
-    <div>
+
+    <header >
+      <h1>The creative crew</h1>
       <h2>Who we are</h2>
-      <section>We are team of creatively diverse.  driven.  innovative individuals working in various locations from the world.</section>
-    </div>
-      <div>
-        <div className='container1'>
-          <img className='peaple1' src={img1} alt="peaple1" />
-          <p className='name1'>Bill Mahoney</p>
-          <span className='profession1'>PRODUCT OWNER</span>
-        </div>
-       
-        <div className='container2'>
-          <img className='peaple2' src={img2} alt="peaple2" />
-          <p classaname='name2'>Saba Cabrera</p>
-          <span className='profession2'>ART DIRECTOR</span>
-        </div>
+      <p>We are team of creatively diverse.  driven.  innovative individuals working in various locations from the world.</p>
+    </header>
+
+      <main>
+
+      <section>
+        <img className='peaple1' src={img1} alt="" /> 
+        <p className='name1'>Bill Mahoney</p>
+        <p className='office1'>PRODUCT OWNER</p>
+
+        <img  className='peaple2' src={img2} alt="" />
+        <p className='name2'>Saba Cabrera</p>
+        <p className='office2'>ART DIRECTOR</p>
+
+        <img className='peaple3' src={img3} alt="" />
+        <p className='name3'>Shae Le</p>
+        <p className='office3'>TECH LEAD</p>
+
+        <img className='peaple4' src={img4} alt="" />
+        <p className='name4'>Skylah Lu</p>
+        <p className='office4'>UX DESIGNER</p>
+
+        <img className='peaple5' src={img5} alt="" />
+        <p className='name5'>Griff Richards</p>
+        <p className='office5'>DEVELOPER</p>
+
+        <img className='peaple6' src={img6} alt="" />
+        <p className='name6'>Stan John</p>
+        <p className='office6'>DEVELOPER</p>
+      </section>
+         
         
-        <div className='container3'> 
-          <img className='peaple3' src={img3} alt="peaple3" />
-          <p className='name3'>Shae Le</p>
-          <span className='profession3'>TECH LEAD</span>
-        </div>
         
-        <div className='container4'>
-          <img className='peaple4' src={img4} alt="peaple4" />
-          <p className='name4'>Skylah Lu</p>
-          <span className='profession4'>UX DESIGNER</span>
-        </div>
-
-        <div className='container5'>
-          <img className='peaple5' src={img5} alt="peaple5" />
-          <p className='name5'>Griff Richards</p>
-          <span className='profession5'>DEVELOPER</span>
-        </div>
-
-        <div className='container6'>
-          <img className='peaple6' src={img6} alt="peaple6" />
-          <p className='name6'>Stan John</p>
-          <span className='profession6'>DEVELOPER</span>
-        </div>
-
-
-      </div>
+      </main>        
 
       <footer>created by - NattanJunior</footer>
+
    </div>
   )
 }
